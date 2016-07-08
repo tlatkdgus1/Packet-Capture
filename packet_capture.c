a7afc808b9e067498128cc5820d6633c685ca6f9
tlatkdgus1 <ipod1597@naver.com>
2016-07-08 11:28:14 +0900

    Packet Capture

--- packet_capture.c
+++ packet_capture.c
@@ -1,4 +1,5 @@
 #include <stdio.h>
+#include <stdlib.h>
 #include <pcap.h>
 #include <netinet/ip.h>
 #include <netinet/tcp.h>
@@ -11,20 +12,27 @@ void packet_handler(u_char *param, const struct pcap_pkthdr *header, const u_cha
 	struct ethhdr *eth;
 	struct iphdr *iph;
 	struct tcphdr *tcph;
-
+	struct ether_header *eh;
+	struct ip *ip;
 	eth = (struct ethhdr *)pkt_data;
-        iph = (struct iphdr *)(pkt_data + sizeof(struct ethhdr));
-        tcph = (struct tcphdr*)(pkt_data + sizeof(struct iphdr) + sizeof(struct ethhdr));
+	eh = (struct ether_header *)pkt_data;
 
-	printf("===========================\n");
-	printf("Device : %s\n", dev);
-	printf("Ether Src : %.2X-%.2X-%.2X-%.2X-%.2X-%.2X \n", eth->h_source[0] , eth->h_source[1] , eth->h_source[2] , eth->h_source[3] , eth->h_source[4] , eth->h_source[5]);
-	printf("Ether Dst : %.2X-%.2X-%.2X-%.2X-%.2X-%.2X \n", eth->h_dest[0] , eth->h_dest[1] , eth->h_dest[2] , eth->h_dest[3] , eth->h_dest[4] , eth->h_dest[5]);
-	printf("IP Src : %s\n", inet_ntoa(*(struct in_addr *)&iph->saddr));
-	printf("IP Dst : %s\n", inet_ntoa(*(struct in_addr *)&iph->daddr));
-	printf("Src port : %u\n",ntohs(tcph->source));
-    	printf("Dst Port : %u\n",ntohs(tcph->dest));
-	printf("===========================\n\n");		
+	if(ntohs(eh->ether_type) == ETHERTYPE_IP){
+		iph = (struct iphdr *)(pkt_data + sizeof(struct ethhdr));
+		ip = (struct ip *)(pkt_data + sizeof(struct ether_header));
+		if(ip->ip_p == IPPROTO_TCP){
+        	tcph = (struct tcphdr*)(pkt_data + (ip->ip_hl) * 4 + sizeof(struct ethhdr));
+			printf("===========================\n");
+			printf("Device : %s\n", dev);
+			printf("Ether Src : %.2X-%.2X-%.2X-%.2X-%.2X-%.2X \n", eth->h_source[0] , eth->h_source[1] , eth->h_source[2] , eth->h_source[3] , eth->h_source[4] , eth->h_source[5]);
+			printf("Ether Dst : %.2X-%.2X-%.2X-%.2X-%.2X-%.2X \n", eth->h_dest[0] , eth->h_dest[1] , eth->h_dest[2] , eth->h_dest[3] , eth->h_dest[4] , eth->h_dest[5]);
+			printf("IP Src : %s\n", inet_ntoa(*(struct in_addr *)&iph->saddr));
+			printf("IP Dst : %s\n", inet_ntoa(*(struct in_addr *)&iph->daddr));
+			printf("Src port : %u\n",ntohs(tcph->source));
+    			printf("Dst Port : %u\n",ntohs(tcph->dest));
+			printf("===========================\n\n");
+		}
+	}		
 }
 
 int main(int argc, char *argv[])
@@ -33,7 +41,17 @@ int main(int argc, char *argv[])
 	pcap_t* handler;
 
 	dev = pcap_lookupdev(errbuf);
+	if(dev == NULL){
+        	printf("%s\n",errbuf);
+        	exit(1);
+    	}
+
 	handler = pcap_open_live(dev, BUFSIZ, 1, 1000, errbuf);
+	if(handler == NULL){
+        	printf("%s\n",errbuf);
+        	exit(1);
+    	}
+
 	pcap_loop(handler, 0, packet_handler,NULL);
 	
     return 0;
